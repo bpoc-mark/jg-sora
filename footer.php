a911a852a796c90636d7ec8e6b48e9b7cf381603
gregarsua <arsuagreg@gmail.com>
2022-02-15 09:24:25 +0800

    Initial commit

---
+++ footer.php
@@ -0,0 +1,12 @@
+        </div>
+        <footer class="page__footer">
+            footer
+        </footer>
+        </main>
+        <!-- INSERT SCRIPTS HERE -->
+        <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+        <!-- END OF SCRIPTS -->
+        <?php wp_footer(); ?>
+        </body>
+
+        </html>
---
+++ page-blog.php
@@ -0,0 +1,29 @@
+<?php
+
+/* Template Name: Blog Page */
+
+get_header();?>
+
+<?php
+            $paged = ( get_query_var( 'paged' ) ) ? absint( get_query_var( 'paged' ) ) : 1;
+            
+            $args = array(
+                'post_type' => 'post',
+                'post_status'=>'publish',
+                'posts_per_page' => 9,
+                'paged' => $paged,
+            );
+            
+            $the_query = new WP_Query($args);
+        ?>
+
+<?php if ( $the_query->have_posts() ) : ?>
+
+<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+<!-- BLOG POST CONTENT -->
+
+<?php endwhile; ?>
+<?php endif; ?>
+
+<?php get_footer();?>
